4e8b8e340831f724109943cc63657b5278fff2db
Whiser <117631755+weax420@users.noreply.github.com>
2024-02-25 13:29:43 +0800

    Update Games.lua

--- Others/Games.lua
+++ Others/Games.lua
@@ -1,1 +1,11 @@
-j
+--[[
+    DO NOT COPY THIS CODE!!
+    PROPERTY AND OWNED BY WHISTHUB (weax_gd) on Discord.
+]]
+local games = {}
+
+if games[game.PlaceId] ~= nil then
+    loadstring(games[tonumber(game.PlaceId)])()
+else
+    game.Players.LocalPlayer:Kick("Game Not Supported! :(")
+end
