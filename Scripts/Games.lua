62baf17bbc9d2522350fd6e86c6899a0874d4ac0
Whiser <117631755+wea-the@users.noreply.github.com>
2024-05-28 08:53:06 +0800

    Update Games.lua

--- Scripts/Games.lua
+++ Scripts/Games.lua
@@ -1,6 +1,6 @@
 --[[
-    DO NOT COPY THIS CODE!!
-    PROPERTY AND OWNED BY CALHUB.
+    This is where all the scripts for the supported games located.
+    All the links here are protected.
 ]]
 local games = {
     [13833961666] = "loadstring(game:HttpGet('https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Eternal-Bob.lua'))()"
