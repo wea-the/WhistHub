2bd8541bcb28a497f7d1e86164096e41aee2400e
Whiser <117631755+wea-the@users.noreply.github.com>
2024-05-28 08:41:56 +0800

    Update CalHub.lua

--- CalHub.lua
+++ CalHub.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Games.lua", true))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/wea-the/CalHub/main/Scripts/Games.lua"))()
